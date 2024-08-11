9c3e067604a8b4b3cf052a8c3424e3b50c28e6fb
lacomic <168654841+lacomic@users.noreply.github.com>
2024-08-11 15:59:49 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html long="ja">
+
+<head>
+    <meta charset="UFT-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 連想配列に値を代入する
+        $vegitable = ['name' => 'onion','price' => '200','weight' => '160'];
+
+        // 連想配列の値を出力する
+        print_r($vegitable);
+      ?>
+    </p>
+</body>
+
+</html>
+
+
+
